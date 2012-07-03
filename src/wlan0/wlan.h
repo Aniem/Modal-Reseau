f1c65fe7ec06f92cb01e7ce1869c28e6620241ad
strelok <svyatoslav.covanov@gmail.com>
2012-07-03 20:10:43 +0200

    modified:   wlan.h

--- src/wlan0/wlan.h
+++ src/wlan0/wlan.h
@@ -1,10 +1,12 @@
 #include "../net/udpsocket.h"
 #include "../net/address.h";
+#include "../core/thread.h";
 
 namespace Modal{
 	class wlan : public Thread {
 		public:
 			int wlan(Modal::TunInterface tun,Modal::ModRoute mod,const std::string& ip, unsigned short port);
+			~wlan();
 			void recevons(void* buf, int size);
 			void sendons(
 			void run();
